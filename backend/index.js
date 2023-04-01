015cafd05d68f0c70d440d8c893a4b0c24885555
Julia <earnkay2@gmail.com>
2023-04-01 21:21:10 +0100

    routed from goalsapp to frontend

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,42 @@
-import express from 'express';
-import * as dotenv from 'dotenv'
-dotenv.config()
-import cors from 'cors'
-import path from 'path';
-import goalRoutes from './routes/goals.js'
-import userRoutes from './routes/users.js'
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import path from "path";
+import goalRoutes from "./routes/goals.js";
+import userRoutes from "./routes/users.js";
 // import { errorHandler } from './middleware/errorMiddleware.js'
-import errorHandler from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import errorHandler from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({extended: false}))
-app.use('/api/goals', goalRoutes)
-app.use('/api/users', userRoutes)
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/goals", goalRoutes);
+app.use("/api/users", userRoutes);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../goals-app/dist')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'goals-app', 'dist', 'index.html')))
-}else{
-    app.get('/', (req,res) => res.send('please set to production'))
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("please set to production"));
 }
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
