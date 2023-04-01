fea02411e5059daa5a70ccccfd1b462f6e78026a
Julia <earnkay2@gmail.com>
2023-04-01 22:35:16 +0100

    fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,14 @@
-import { fileURLToPath, URL } from "node:url";
-// import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-const defineConfig = {
+export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-};
-
-export default defineConfig;
+});
